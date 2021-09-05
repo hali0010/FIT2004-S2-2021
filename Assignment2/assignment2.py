8c043f7f4578b8f3b6353dd99eb0c371922626b3
Haider Ali <hali0010@student.monash.edu>
2021-09-05 11:41:44 +1000

    added week5 studio stuff

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,51 +1,34 @@
 # Student Name: Haider Ali
 # Student ID: 31371337 
 
-import math
-
 # Task 1 Game Master
-
-# def min_coins(coins,V):
-#     coins_needed = [math.inf] * (V+1)
-#     coins_needed[0] = 0
-#     for i in range(1,V+1):
-#         if i>= min(coins):
-#             best_so_far = math.inf
-#         for c in coins:
-#             if c<=i:
-#                 best_so_far = min(best_so_far,1+coins_needed[i-c])
-#         coins_needed[i] = best_so_far
-#     return coins_needed[V]
-
-def min_coins_TD(coins,V):
-    coins_needed = [math.inf] * (V+1)
-    output = min_coins_aux(coins,V,coins_needed)
-    return output
-
-def min_coins_aux(coins,V,coins_needed):
-    if V ==0 :
-        coins_needed[V] = 0
-        return 0
-    if coins_needed[V] == None:
-        best_so_far = math.inf
-        for c in coins:
-            if c<=V:
-                best_so_far = min(best_so_far,1+min_coins_aux(c,V-c,coins_needed))        
-        coins_needed[V] = best_so_far
-
-    return coins_needed[V]
-
+# count_aux has been modified from https://www.reddit.com/r/learnprogramming/comments/83vrpb/print_all_combination_of_coin_change_problem_i_am/
+def count_aux(monster_difficulties, goal):
+    memo_monster_diff = [[monster_dif] for monster_dif in monster_difficulties] # a memo array to store the monster difficulties
+    arr = [] # an empty array for helping with managing the memo array
+    final_memo = []
+    count = 0
+    while memo_monster_diff:
+        for existing_monster_difficulties in memo_monster_diff:
+            sum_to_include_monster_diff = sum(existing_monster_difficulties) # adds the similiar difficulties together for example adds the difficulty of (2) and (3) along with the difficulty of (5)
+            for monster_dif in monster_difficulties:
+                if monster_dif >= existing_monster_difficulties[-1]:
+                    if sum_to_include_monster_diff + monster_dif < goal:
+                        arr.append(existing_monster_difficulties + [monster_dif])
+                    elif sum_to_include_monster_diff + monster_dif == goal:
+                        final_memo.append(existing_monster_difficulties + [monster_dif])
+        memo_monster_diff = arr
+        arr = []
+    for i in range(0,len(final_memo)):
+        count = count +1
+    return count
 
 def count_encounters(target_difficulty,monster_list):
     monster_difficulties = [ j for i, j in monster_list]
-    best_outcomes = min_coins_TD(monster_difficulties,target_difficulty)
-    max = best_outcomes[0]
-    for i in range(1,len(best_outcomes)):
-        if max < best_outcomes[i]:
-            max = best_outcomes[i]
-    return max
+    return count_aux(monster_difficulties,target_difficulty)
 
 
 target_difficulty = 15
 monster_list = [("bear", 5), ("imp", 2), ("kobold", 3), ("dragon", 10)]
 print(count_encounters(target_difficulty, monster_list))
+print(count_aux([1,2,3],5))
--- Studio/studio5.py
+++ Studio/studio5.py
@@ -1,18 +1,19 @@
 import math
 def min_coins(coins,V):
-    coins_needed = [math.inf] * (V+1)
+    coins_needed = [-1] * (V+1)
     coins_needed[0] = 0
     for i in range(1,V+1):
         if i>= min(coins):
-            best_so_far = math.inf
-        for c in coins:
-            if c<=i:
-                best_so_far = min(best_so_far,1+coins_needed[i-c])
-        coins_needed[i] = best_so_far
+            best_so_far = 0
+            for c in coins:
+                if c<=i:
+                    best_so_far = max(best_so_far,1+coins_needed[i-c])
+            coins_needed[i] = best_so_far
+    print(coins_needed)
     return coins_needed[V]
 
 def min_coins_TD(coins,V):
-    coins_needed = [math.inf] * (V+1)
+    coins_needed = [None] * (V+1)
     output = min_coins_aux(coins,V,coins_needed)
     return output
 
@@ -21,10 +22,13 @@ def min_coins_aux(coins,V,coins_needed):
         coins_needed[V] = 0
         return 0
     if coins_needed[V] == None:
-        best_so_far = math.inf
+        best_so_far = -1
         for c in coins:
             if c<=V:
-                best_so_far = min(best_so_far,1+min_coins_aux(c,V-c,coins_needed))        
+                best_so_far = max(best_so_far,1+min_coins_aux(c,V-c,coins_needed))        
         coins_needed[V] = best_so_far
 
     return coins_needed[V]
+
+coins = [5,2,3,10]
+print(min_coins_TD(coins,15))
