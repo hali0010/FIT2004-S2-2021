5b6386c6fbcd6dede6be2ecdac461ad0cbb39734
Haider Ali <hali0010@student.monash.edu>
2021-09-08 22:34:50 +1000

    finished assignment 2.py. needs comments

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -31,30 +31,22 @@ def count_encounters(target_difficulty,monster_list):
 
 def best_lamp_allocation(num_p , num_l,probs):
     memo_greenhouse = [[0 for i in range(num_l+1)] for j in range(num_p)]
-    lamp_left = num_l
-    lamps_taken = 0
     for l in range(0,num_l+1):
         memo_greenhouse[0][l] = probs[0][l]
     for plants in range(1,num_p):    
         for lamps in range(0,num_l+1): # we need to find the optimum number of lamps for each plant
-            lamp_left = num_l - lamps  # lamp_left is the number of lamps we can use
-            max = probs[plants][lamps]*memo_greenhouse[plants-1][lamp_left]
-            if (lamp_left>0):
-               for l in range(lamp_left-1,-1,-1): # 1 -> 0 
-                   temp = probs[plants][lamps]*memo_greenhouse[plants-1][l]  # 
-                   if temp > max:
-                       max = temp
-                       lamps_taken =l
-              # for the situations we cannot use any lamp for that plant
+            if lamps>0:
+                temp = [0]*(lamps+1)
+                for l in range(0,lamps+1):
+                    temp[l]= probs[plants][lamps - l] * memo_greenhouse[plants -1][l]
+                maxP = max(temp)
+                maxP = max(memo_greenhouse[plants][lamps-1],maxP)
+                memo_greenhouse[plants][lamps] = maxP
+            else:
+                memo_greenhouse[plants][lamps] = probs[plants][lamps]*memo_greenhouse[plants -1][lamps]
 
-            memo_greenhouse[plants][lamps]= max
-            print(memo_greenhouse)
-    max = memo_greenhouse[num_p-1][0]
-    for lamps in range(0,num_l+1):
-        if(memo_greenhouse[num_p-1][lamps]>max):
-            max = memo_greenhouse[num_p-1][lamps]
-    return max
-    
+    return memo_greenhouse[num_p-1][num_l]
+
 # test case 1    
 # probs = [[0.92, 0.88, 0.07, 0.74, 0.83, 0.73, 0.85, 0.41, 0.94, 0.58, 0.17],
 # [0.05, 0.42, 0.01, 0.53, 0.03, 0.13, 0.49, 0.64, 0.13, 0.78, 0.05],
@@ -75,18 +67,18 @@ def best_lamp_allocation(num_p , num_l,probs):
 
 # test case 2 
 
-# probs = [[0.39, 0.53, 0.09, 0.13, 0.36, 0.91, 0.84, 0.14, 0.3, 0.23, 0.21],
-#  [0.31, 0.49, 0.99, 0.13, 0.45, 0.7, 0.73, 0.22, 0.97, 0.89, 0.93],
-#  [0.08, 0.73, 0.17, 0.24, 0.62, 0.69, 0.43, 0.31, 0.79, 0.73, 0.96],
-#  [0.42, 0.1, 0.97, 0.27, 0.5, 0.84, 0.32, 0.53, 0.31, 0.22, 0.93],
-#  [0.45, 0.51, 0.99, 0.86, 0.22, 0.62, 0.45, 0.47, 0.83, 0.88, 0.85],
-#  [0.68, 0.35, 0.5, 0.06, 0.14, 0.88, 0.51, 0.84, 0.35, 0.12, 0.38],
-#  [0.86, 0.64, 0.78, 0.17, 0.24, 0.69, 0.4, 0.72, 0.74, 0.14, 0.97],
-#  [0.48, 0.02, 0.48, 0.09, 0.73, 0.37, 0.68, 0.34, 0.49, 0.28, 0.37],
-#  [0.69, 0.25, 0.46, 0.2, 0.68, 0.73, 0.83, 0.26, 0.92, 0.74, 0.97],
-#  [1.0, 0.57, 0.77, 0.55, 0.79, 0.54, 0.07, 0.89, 0.38, 0.55, 0.87]]
-# num_l = 10
-# num_p = 10
+probs = [[0.39, 0.53, 0.09, 0.13, 0.36, 0.91, 0.84, 0.14, 0.3, 0.23, 0.21],
+ [0.31, 0.49, 0.99, 0.13, 0.45, 0.7, 0.73, 0.22, 0.97, 0.89, 0.93],
+ [0.08, 0.73, 0.17, 0.24, 0.62, 0.69, 0.43, 0.31, 0.79, 0.73, 0.96],
+ [0.42, 0.1, 0.97, 0.27, 0.5, 0.84, 0.32, 0.53, 0.31, 0.22, 0.93],
+ [0.45, 0.51, 0.99, 0.86, 0.22, 0.62, 0.45, 0.47, 0.83, 0.88, 0.85],
+ [0.68, 0.35, 0.5, 0.06, 0.14, 0.88, 0.51, 0.84, 0.35, 0.12, 0.38],
+ [0.86, 0.64, 0.78, 0.17, 0.24, 0.69, 0.4, 0.72, 0.74, 0.14, 0.97],
+ [0.48, 0.02, 0.48, 0.09, 0.73, 0.37, 0.68, 0.34, 0.49, 0.28, 0.37],
+ [0.69, 0.25, 0.46, 0.2, 0.68, 0.73, 0.83, 0.26, 0.92, 0.74, 0.97],
+ [1.0, 0.57, 0.77, 0.55, 0.79, 0.54, 0.07, 0.89, 0.38, 0.55, 0.87]]
+num_l = 10
+num_p = 10
 
 #solution
 #allocation of lamps to plants: [1, 2, 1, 2, 2, 0, 0, 0, 0, 0] 
@@ -112,14 +104,14 @@ def best_lamp_allocation(num_p , num_l,probs):
 
 # Test case 2
 
-num_p = 4
+# num_p = 4
 
-num_l = 3
+# num_l = 3
 
-probs = [[0.2,0.3,0.1,0.2], [0.7, 0.6, 0.4,0.3], [0.1,1,0.9,0.92], [0.6,0.5,0.2,0.8]]
+# probs = [[0.2,0.3,0.1,0.2], [0.7, 0.6, 0.4,0.3], [0.1,1,0.9,0.92], [0.6,0.5,0.2,0.8]]
 
 # result: 0.288
-
+print(probs)
 print(best_lamp_allocation(num_p,num_l,probs))
 
 
